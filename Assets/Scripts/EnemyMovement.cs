df8811a9d47d1cad3fb666160b059728e5bf53a8
Bertram Bryde Holmsteen <diannnbryde@gmail.com>
2023-02-06 16:34:33 -0800

    Tilføjet Gloop og hans venner

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class EnemyMovement : MonoBehaviour
 {
@@ -13,29 +14,49 @@ public class EnemyMovement : MonoBehaviour
     [SerializeField] private float speed = 1;
     [SerializeField] private float jumpStrenght = 20;
 
+    [SerializeField] private bool resetVel;
+    [SerializeField] private bool chaseTarget;
+
     private float timer;
     
 
     private void Update()
     {
-        Vector3 direction = target.position - this.transform.position;
-        Vector3 force = new Vector3(direction.x * speed, direction.y + jumpStrenght, direction.z * speed);
-        
-        timer += Time.deltaTime;
-        
-        if (timer >= 1)
+        if (chaseTarget)
         {
-            rb.velocity = Vector3.zero;
-            rb.AddForce(force * 5);
-            timer = 0;
+            Vector3 direction = target.position - this.transform.position;
+        
+            direction.x = Mathf.Clamp(direction.x, -5f, 5f);
+            direction.y = Mathf.Clamp(direction.y, -5f, 5f);
+            direction.z = Mathf.Clamp(direction.z, -5f, 5f);
+        
+            Vector3 force = new Vector3(direction.x * speed, direction.y + jumpStrenght, direction.z * speed);
+        
+            //Debug.Log(direction);
+        
+            timer += Time.deltaTime;
+        
+            float value = 0;
+            value = Random.Range(0.8f, 1f);
+        
+            if (timer >= value)
+            {
+                if (resetVel)
+                {
+                    rb.velocity = Vector3.zero;
+                }
+            
+                rb.AddForce(force * 5);
+                timer = 0;
+            }
         }
     }
 
 
-    private void OnDrawGizmosSelected()
+    private void OnDrawGizmos()
     {
         Vector3 direction = target.position - this.transform.position;
-        Debug.Log(direction);
+        //Debug.Log(direction);
         Gizmos.DrawRay(transform.position, direction);
     }
 }
