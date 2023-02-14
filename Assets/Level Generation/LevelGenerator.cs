7fd6c0f08f438dcb97b151a6d9771bf42d4885b7
Mikkel BLom <mikkel@ryborgblom.dk>
2023-02-14 14:11:12 -0800

    Fade fix

--- Assets/Level Generation/LevelGenerator.cs
+++ Assets/Level Generation/LevelGenerator.cs
@@ -89,7 +89,10 @@ public class LevelGenerator : MonoBehaviour
 
         for (int i = 0; i < throwableObjects.Length; i++)
         {
-            Destroy(throwableObjects[i]);
+            if (throwableObjects[i].transform != player.GetComponent<CharacterController>().heldObject)
+            {
+                Destroy(throwableObjects[i]);
+            }
         }
 
         if (!player)
@@ -235,9 +238,8 @@ public class LevelGenerator : MonoBehaviour
 
         while (true)
         {
-            if (intersects && GetBoundsRaw(player).Intersects(GetBoundsRaw(currentRooms[currentRooms.Count - 1])))
+            if (GetBoundsRaw(player).Intersects(GetBoundsRaw(currentRooms[currentRooms.Count - 1])))
             {
-                intersects = false;
                 AudioManager.instance.SetParameter("ElevatorLoad", 1f);
                 AudioManager.instance.SetParameter("Elevator", 0);
                 AudioManager.instance.SetParameter("Situation", 2);
@@ -251,9 +253,11 @@ public class LevelGenerator : MonoBehaviour
                 createNewRoom = false;
 
                 FadeToBlack(0.3f);
-                yield return new WaitForSeconds(0.7f);
+                yield return new WaitForSeconds(1f);
                 minEnemyCount += minEnemyCountIncrease;
                 maxEnemyCount += maxEnemyCountIncrease;
+                Debug.Log("Fade");
+                yield return new WaitForSeconds(1);
                 CreateLevel();
 
                 AudioManager.instance.SetParameter("ElevatorLoad", 1f);
