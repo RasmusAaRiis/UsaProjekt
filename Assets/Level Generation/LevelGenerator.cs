82dc69971fe90dc17b61311ba1e41a1b8ef1fb43
Mikkel BLom <mikkel@ryborgblom.dk>
2023-02-14 09:56:13 -0800

    Levelgen update (Test)

--- Assets/Level Generation/LevelGenerator.cs
+++ Assets/Level Generation/LevelGenerator.cs
@@ -246,6 +246,13 @@ public class LevelGenerator : MonoBehaviour
                 minEnemyCount += minEnemyCountIncrease;
                 maxEnemyCount += maxEnemyCountIncrease;
                 CreateLevel();
+<<<<<<< Updated upstream
+=======
+                for (int i = 0; i < 1000; i++)
+                {
+                    Debug.Log("TEST: " + i);
+                }
+>>>>>>> Stashed changes
                 //Generate nyt rum
 
                 //Elevatorload mod 0
