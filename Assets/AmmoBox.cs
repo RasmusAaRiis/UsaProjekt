5607b3c95f71ee071dad7278c72ad11a2a78e31b
Rasmus Riis <rasmus.aagot.riis@gmail.com>
2023-02-15 23:58:35 +0100

    Alt

--- Assets/AmmoBox.cs
+++ Assets/AmmoBox.cs
@@ -7,12 +7,24 @@ public class AmmoBox : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         Stapler stapler;
+        FireExtinguisher fireExtinguisher;
         if (other.transform.GetComponentInChildren<Stapler>())
         {
+            Debug.Log("Go");
             stapler = other.transform.GetComponentInChildren<Stapler>();
             CharacterController cc = other.transform.GetComponent<CharacterController>();
             int appliedAmmo = 20 * cc.ammoModifier;
             stapler.ammo += appliedAmmo;
+            cc.PickupText("+" + appliedAmmo + " Ammo");
+            Destroy(gameObject);
+            Debug.Log("Go");
+        }
+        if (other.transform.GetComponentInChildren<FireExtinguisher>())
+        {
+            fireExtinguisher = other.transform.GetComponentInChildren<FireExtinguisher>();
+            CharacterController cc = other.transform.GetComponent<CharacterController>();
+            int appliedAmmo = 10 * cc.ammoModifier;
+            fireExtinguisher.ammo += appliedAmmo;
             cc.PickupText("+" + appliedAmmo + " Ammo");
             Destroy(gameObject);
         }
