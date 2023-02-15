15578cdc2c1134942c76a793f8ed9980a59054d2
Bertram Bryde Holmsteen <diannnbryde@gmail.com>
2023-02-15 12:26:03 -0800

    dør bliver sparket op nu

--- Assets/Scripts/DoorBehavior.cs
+++ Assets/Scripts/DoorBehavior.cs
@@ -7,13 +7,17 @@ public class DoorBehavior : MonoBehaviour
 {
     [SerializeField] private ConfigurableJoint cj;
 
+    [SerializeField] private Rigidbody rb;
+
     private void Start()
     {
         cj = GetComponentInChildren<ConfigurableJoint>();
+        rb = GetComponentInChildren<Rigidbody>();
     }
 
     public void OpenDoor()
     {
         cj.angularYMotion = ConfigurableJointMotion.Limited;
+        rb.AddForce(transform.right * 20000, ForceMode.Impulse);
     }
 }
