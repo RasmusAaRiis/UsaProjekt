ba44efb30afef2b650af5876c51b7688533bd006
mastermokkel <mastermikkel03@gmail.com>
2023-02-16 23:06:16 +0100

    buttonButton

--- Assets/Scripts/FMODEvents.cs
+++ Assets/Scripts/FMODEvents.cs
@@ -24,6 +24,7 @@ public class FMODEvents : MonoBehaviour
     [field: SerializeField] public EventReference drink { get; private set; }
     [field: SerializeField] public EventReference victory { get; private set; }
     [field: SerializeField] public EventReference spray { get; private set; }
+    [field: SerializeField] public EventReference buttonButton { get; private set; }
 
 
     public static FMODEvents instance { get; private set; }
