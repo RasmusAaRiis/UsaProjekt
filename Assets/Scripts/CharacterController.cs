b00f545ec96189b01f10f8ddcdb045d9197d15a1
Rasmus Riis <rasmus.aagot.riis@gmail.com>
2023-02-17 21:24:04 +0100

    NU

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -259,7 +259,7 @@ public class CharacterController : MonoBehaviour
                     lookedAtObject.GetComponentInParent<Animator>().SetTrigger("Open");
                 }
                 lookedAtObject.tag = "Untagged";
-                lookedAtObject.GetComponent<Rigidbody>().isKinematic = false;
+                lookedAtObject.transform.GetChild(0).GetComponent<Rigidbody>().isKinematic = false;
                 AudioManager.instance.PlayOneShot(FMODEvents.instance.openDoor, this.transform.position);
             }
 
