70711b53859c1abd8c54bf733bd8ecde551de9ac
Bertram Bryde Holmsteen <diannnbryde@gmail.com>
2023-02-21 11:28:23 -0800

    Skift på kvalitets settings og inverse kinematics

--- Assets/Scripts/FastIKFabric.cs
+++ Assets/Scripts/FastIKFabric.cs
@@ -186,18 +186,18 @@ namespace DitzelGames.FastIK // The tutorial guy :DDDD
                 current.rotation = Root.rotation * rotation;
         }
         
-        void OnDrawGizmosSelected()
-        {
-            var current = this.transform;
-            for (int i = 0; i < ChainLength && current != null && current.parent != null; i++)
-            {
-                var scale = Vector3.Distance(current.position, current.parent.position) * 0.1f;
-                Handles.matrix = Matrix4x4.TRS(current.position, Quaternion.FromToRotation(Vector3.up, current.parent.position - current.position), new Vector3(scale, Vector3.Distance(current.parent.position, current.position), scale));
-                Handles.color = Color.green;
-                Handles.DrawWireCube(Vector3.up * 0.5f, Vector3.one);
-                current = current.parent;
-            }
-        }
+        // void OnDrawGizmosSelected()
+        // {
+        //     var current = this.transform;
+        //     for (int i = 0; i < ChainLength && current != null && current.parent != null; i++)
+        //     {
+        //         var scale = Vector3.Distance(current.position, current.parent.position) * 0.1f;
+        //         Handles.matrix = Matrix4x4.TRS(current.position, Quaternion.FromToRotation(Vector3.up, current.parent.position - current.position), new Vector3(scale, Vector3.Distance(current.parent.position, current.position), scale));
+        //         Handles.color = Color.green;
+        //         Handles.DrawWireCube(Vector3.up * 0.5f, Vector3.one);
+        //         current = current.parent;
+        //     }
+        // }
         
     }
 }
